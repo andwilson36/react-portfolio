9ae951d0fe2a09654a9ea4873ab26053c2506538
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-08-23 17:25:30 -0500

    added hero animation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">    
+      <section></section>
+        <div className="hero-background spacer">
+          <h1 className="name-header">Andrew Wilson</h1>
+        </div>
+      <section></section>
+    </div>
+  );
+}
+
+export default App;
