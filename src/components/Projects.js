f1e4fe99084d51aece0efbac56d259c04e5caab9
andwilson36 <wilsonandrew57@gmail.com>
2021-08-26 15:27:47 -0500

    project carousel

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,56 @@
 import React from 'react'
 import longRides from '../assets/long-rides.png';
+import ticket from '../assets/ticket.png';
 
 
 function Projects() {
     return (
         <div>
             <div className="title">Projects</div>
-            <div className="container">
-                <img className="long-rides" src={longRides} />
+            <div className="carousel-container col-8">
+                <div id="carouselProjects" className="carousel slide" data-bs-ride="carousel">
+                    <div className="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselProjects" data-bs-slide-to="0" className="active"
+                            aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselProjects" data-bs-slide-to="1"
+                            aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselProjects" data-bs-slide-to="2"
+                            aria-label="Slide 3"></button>
+                    </div>
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src={longRides} className="d-block w-100 project" alt="..." />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src="..." className="d-block w-100 project" alt="..." />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the second slide.</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src="..." className="d-block w-100 project" alt="..." />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Some representative placeholder content for the third slide.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselProjects"
+                        data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselProjects"
+                        data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
             </div>
         </div>
     )
