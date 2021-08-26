4dc03d53789570d7e1bc4d8813aff49bc4c6bbb0
andwilson36 <wilsonandrew57@gmail.com>
2021-08-26 16:20:52 -0500

    finished project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,56 +1,51 @@
-import React from 'react'
+import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 import longRides from '../assets/long-rides.png';
 import ticket from '../assets/ticket.png';
+import weather from '../assets/weather.png';
 
 
 function Projects() {
     return (
         <div>
             <div className="title">Projects</div>
-            <div className="carousel-container col-8">
-                <div id="carouselProjects" className="carousel slide" data-bs-ride="carousel">
-                    <div className="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselProjects" data-bs-slide-to="0" className="active"
-                            aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselProjects" data-bs-slide-to="1"
-                            aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselProjects" data-bs-slide-to="2"
-                            aria-label="Slide 3"></button>
+            <div className="carousel-container">
+                <Carousel>
+                    <div>
+                        <img src={longRides} />
+                        <p className="legend">
+                            <strong>Long Rides</strong>
+                            <br></br>
+                            Long Rides is an application that allows UT students to have an affordable and convenient rideshare system.
+                            <br></br>
+                            <a href="https://github.com/andwilson36/Long_Rides_project_two" target="_blank">Deployment</a>&nbsp;&nbsp;|&nbsp;&nbsp;  
+                            <a href="https://github.com/andwilson36/Long_Rides_project_two" target="_blank">GitHub Repo</a>
+                        </p>
                     </div>
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img src={longRides} className="d-block w-100 project" alt="..." />
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>First slide label</h5>
-                                <p>Some representative placeholder content for the first slide.</p>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img src="..." className="d-block w-100 project" alt="..." />
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>Second slide label</h5>
-                                <p>Some representative placeholder content for the second slide.</p>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img src="..." className="d-block w-100 project" alt="..." />
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>Third slide label</h5>
-                                <p>Some representative placeholder content for the third slide.</p>
-                            </div>
-                        </div>
+                    <div>
+                        <img src={ticket} />
+                        <p className="legend">
+                            <strong>Ticket</strong>
+                            <br></br>
+                            Ticket is a website where you can search anywhere in the world for flights and hotels and see quotes for the areas you want.
+                            <br></br>
+                            <a href="https://andwilson36.github.io/Ticket_Project_One/" target="_blank">Deployment</a>&nbsp;&nbsp;|&nbsp;&nbsp;  
+                            <a href="https://github.com/andwilson36/Ticket_Project_One" target="_blank">GitHub Repo</a>
+                        </p>
                     </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselProjects"
-                        data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselProjects"
-                        data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
-                </div>
+                    <div>
+                        <img src={weather} />
+                        <p className="legend">
+                            <strong>Weather Dashboard</strong>
+                            <br></br>
+                            This weather dashboard will take input from users choice of city and display the next five days forecast including, temperature, wind speed, humidity and UV index.
+                            <br></br>
+                            <a href="https://andwilson36.github.io/Weather_Dashboard/" target="_blank">Deployment</a>&nbsp;&nbsp;|&nbsp;&nbsp;  
+                            <a href="https://github.com/andwilson36/Weather_Dashboard" target="_blank">GitHub Repo</a>
+                        </p>
+                    </div>
+                </Carousel>
             </div>
         </div>
     )
